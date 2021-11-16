660fcdcf8fc02fcdabc579c852d21feebfc844b5
Karolinasjokvist <83401567+Karolinasjokvist@users.noreply.github.com>
2021-11-16 16:11:07 +0100

    tw3 update

--- js/DinnerModel.js
+++ js/DinnerModel.js
@@ -1,29 +1,68 @@
 class DinnerModel {
-    constructor(guests = 2, dishes=[], currentDish=null) {
+    constructor(guests = 2, dishes = [], currentDish = null) {
+        this.observers = [];
         this.setNumberOfGuests(guests);
         this.dishes = dishes;
         this.currentDish = currentDish;
     }
 
     setNumberOfGuests(x) {
+        if (x != this.numberOfGuests) {
+            this.notifyObservers();
+        }
+
         if (x <= 0 || typeof (x) !== "number") {
             throw "error";
         } else {
             this.numberOfGuests = x;
         }
+
+    }
+
+    addedToMenu(dish) {
+        this.dishes.forEach(addedDish => {
+            if (addedDish === dish) {
+                return true;
+            }
+        });
+
+        return false;
     }
 
     addToMenu(dish) {
+        if (!this.addedToMenu(dish)) {
+            this.notifyObservers();
+        }
         this.dishes = [... this.dishes, dish];
         this.currentDish = dish;
     }
 
     removeFromMenu(dishData) {
+        if(!this.addedToMenu(dishData)){
+            this.notifyObservers();
+        }
         this.dishes = this.dishes.filter(dish => dish.id !== dishData);
     }
 
     setCurrentDish(id) {
+        if(this.currentDish.id != id){
+            this.notifyObservers();
+        }
         this.currentDish.id = id;
+    }
+
+    addObserver(callback) {
+        this.observers = [... this.observers, callback];
+    }
+
+    removeObserver(callback) {
+        this.observers = this.observers.filter(obs => obs !== callback);
+    }
+
+    notifyObservers() {
+        this.observers.forEach(cb => {
+            try { cb() } catch { }
+        });
     }
 }
 
